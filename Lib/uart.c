f35bc0302c04c684b94571d6bceb8f99e4c7a3a9
Itachi <minhthuan1091996@gmail.com>
2021-11-25 07:18:50 +0700

    add: uart driver

---
+++ Lib/uart.c
@@ -0,0 +1,107 @@
+/****************************************************************************
+* Title                 :   Logger
+* Filename              :   logger.h
+* Author                :   Itachi
+* Origin Date           :   Nov 22, 2021
+* Version               :   1.0.0
+* Compiler              :   TI
+* Target                :   TM4C123
+* Notes                 :   None
+*****************************************************************************/
+
+/*************** INTERFACE CHANGE LIST **************************************
+*
+*    Date       Software Version    Initials        Description
+*  Nov 22, 2021    XXXXXXXXXXX      Itachi      Interface Created.
+*
+*****************************************************************************/
+/** \file XXX.c
+ * \brief This module contains the
+ */
+/******************************************************************************
+* Includes
+
+*******************************************************************************/
+#include "uart.h"
+#include "uartstdio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_memmap.h"
+/******************************************************************************
+* Module Preprocessor Constants
+*******************************************************************************/
+#define RX0_PIN          GPIO_PIN_0
+#define TX0_PIN          GPIO_PIN_1
+/******************************************************************************
+* Module Preprocessor Macros
+*******************************************************************************/
+
+/******************************************************************************
+* Module Typedefs
+*******************************************************************************/
+
+/******************************************************************************
+* Module Variable Definitions
+*******************************************************************************/
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+
+/******************************************************************************
+* Static Function Definitions
+*******************************************************************************/
+void uart0_mspinit(void)
+{
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	GPIOPinConfigure(GPIO_PA0_U0RX);            //PIN mux for UART
+	GPIOPinConfigure(GPIO_PA1_U0TX);            //PIN mux for UART
+	GPIOPinTypeUART(GPIO_PORTA_BASE, RX0_PIN |TX0_PIN);
+}
+
+/******************************************************************************
+* Function Definitions
+*******************************************************************************/
+void uart_ctor(uart_t* me, uint8_t instace, uart_config_t* p_config)
+{
+	me->instance		= instace;
+	me->config.baudrate = p_config->baudrate;
+	me->config.datalen	= p_config->datalen;
+	me->config.parity	= p_config->parity;
+	me->config.stop_bit	= p_config->stop_bit;
+}
+
+bool uart_init(uart_t* me)
+{
+	UARTStdioConfig(me->instance, me->config.baudrate, SysCtlClockGet() );
+	switch (me->instance)
+	{
+		case UART0_INSTANCE:
+		{
+			uart0_mspinit();
+		}
+			break;
+		case UART1_INSTANCE:
+			break;
+		case UART2_INSTANCE:
+			break;
+		default:
+			break;
+	}
+	return true;
+}
+
+bool uart_send(uart_t* p_uart,  const uint8_t* p_data, uint32_t len)
+{
+	UARTwrite((const char*)p_data, len);
+	return true;
+}
+
+bool uart_recv(uart_t* p_uart,  uint8_t* p_buffer, uint32_t len)
+{
+	UARTgets((char*)p_buffer, len);
+	return true;
+}
+
+/*************** END OF FUNCTIONS ***************************************************************************/
---
+++ Lib/uart.h
@@ -0,0 +1,105 @@
+/****************************************************************************
+* Title                 :   ADC Application
+* Filename              :   adc_app.h
+* Author                :   Itachi
+* Origin Date           :   06/07/2012
+* Version               :   1.0.0
+* Compiler              :   TI
+* Target                :   TM4C123
+* Notes                 :   None
+*****************************************************************************/
+
+/*************** INTERFACE CHANGE LIST **************************************
+*
+*    Date       Software Version    Initials        Description
+*  Nov 22, 2021    XXXXXXXXXXX      Itachi      Interface Created.
+*
+*****************************************************************************/
+/** \file tsk.h
+ *  \brief This module contains continuous tasks.
+ *
+ *  This is the header file for the definition for tasks that are continuous
+ *  and should be ran as often as possible.
+ */
+#ifndef UART_H
+#define UART_H
+
+/******************************************************************************
+* Includes
+*******************************************************************************/
+#include "stdbool.h"
+#include "stdint.h"
+/******************************************************************************
+* Preprocessor Constants
+*******************************************************************************/
+#define	UART0_INSTANCE		0
+#define	UART1_INSTANCE		1
+#define	UART2_INSTANCE		2
+
+
+/******************************************************************************
+* Configuration Constants
+*******************************************************************************/
+
+
+/******************************************************************************
+* Macros
+*******************************************************************************/
+
+
+
+/******************************************************************************
+* Typedefs
+*******************************************************************************/
+typedef enum
+{
+	DATA_LEN_7B = 0,
+	DATA_LEN_8B,
+	DATA_LEN_9B
+}eUART_data_len_t;
+
+typedef enum
+{
+	STOP_BIT_NONE = 0,
+	STOP_BIT_1B,
+	STOP_BIT_2B
+}eUART_stop_bit_t;
+
+
+typedef enum
+{
+	PARITY_NONE =0,
+	PARITY_EVEN,
+	PARITY_ODD
+}eUART_parity_t;
+
+typedef struct
+{
+	eUART_data_len_t		datalen;
+	eUART_stop_bit_t		stop_bit;
+	eUART_parity_t			parity;
+	uint32_t				baudrate;
+}uart_config_t;
+
+typedef struct
+{
+	uint8_t 				instance;
+	uart_config_t  			config;
+}uart_t;
+
+/******************************************************************************
+* Variables
+*******************************************************************************/
+
+
+/******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+void uart_ctor(uart_t* p_uart, uint8_t instace, uart_config_t* p_config);
+bool uart_init(uart_t* p_uart);
+bool uart_send(uart_t* p_uart, const uint8_t* p_data, uint32_t len);
+bool uart_recv(uart_t* p_uart, uint8_t* p_buffer, uint32_t len);
+
+#endif /*UART_H*/
+
+/*** End of File **************************************************************/
